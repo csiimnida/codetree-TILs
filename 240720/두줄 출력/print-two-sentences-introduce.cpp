b12de93f6c6ac145ef930fa005105d9898e20de3
크시 <155462542+csiimnida@users.noreply.github.com>
2024-07-20 19:08:47 +0900

    Title: 두줄 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240720/두줄 출력/print-two-sentences-introduce.cpp
+++ 240720/두줄 출력/print-two-sentences-introduce.cpp
@@ -3,6 +3,6 @@ using namespace std;
 
 int main() {
     // 여기에 코드를 작성해주세요.
-    cout << "Hello students!\nWelcome to LeebrosCode!"
+    cout << "Hello students!\nWelcome to LeebrosCode!";
     return 0;
 }

8d5bea253befe4c92a1dd013ab3384e890a56d02
크시 <155462542+csiimnida@users.noreply.github.com>
2024-07-22 20:42:48 +0900

    Title: 합과 평균의 차, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240722/합과 평균의 차/sub-of-average-and-sum.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a,b,c;
+    cin >> a >> b >> c;
+    cout << a+b+c << "\n" << (a+b+c)/2 << (a+b+c) - ((a+b+c)/2);
+    return 0;
+}

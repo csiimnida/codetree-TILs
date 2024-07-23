aac7d75ea74ecefac37041781facd52fd376ef60
크시 <155462542+csiimnida@users.noreply.github.com>
2024-07-23 23:51:24 +0900

    Title: 4번의 크기 비교, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240723/4번의 크기 비교/4-time-comparison.cpp
+++ 240723/4번의 크기 비교/4-time-comparison.cpp
@@ -4,6 +4,7 @@ using namespace std;
 int main() {
     // 여기에 코드를 작성해주세요.
     int a,b,c,d,e;
+    cin >> a >> b >> c >> d >> e;
     cout << (a > b);
     cout << "\n" << (a > b);
     cout << "\n" << (a > d);

000b08be7c57c74993b8db82757bb891e4b46482
크시 <155462542+csiimnida@users.noreply.github.com>
2024-07-23 23:10:47 +0900

    Title: 최대 2번의 연산, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240723/최대 2번의 연산/up-to-2-calculations.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a,b;
+    cin >> a;
+    if (a % 2 == 0){
+        a/=2;
+    }else{
+        a= (a+1)/2;
+    }
+    cout << a;
+    return 0;
+}

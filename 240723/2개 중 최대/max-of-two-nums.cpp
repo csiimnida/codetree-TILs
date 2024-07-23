42a3b2429fea0a3d0d0b4a3d5dca5b358527394a
크시 <155462542+csiimnida@users.noreply.github.com>
2024-07-23 22:41:21 +0900

    Title: 2개 중 최대, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 240723/2개 중 최대/max-of-two-nums.cpp
+++ 240723/2개 중 최대/max-of-two-nums.cpp
@@ -4,8 +4,10 @@ using namespace std;
 int main() {
     // 여기에 코드를 작성해주세요.
     int a,b;
+    int c;
 
     cin >> a >> b;
-    cout << a > b ? a :b;
+    c = a > b ? a :b
+    cout << c;
     return 0;
 }

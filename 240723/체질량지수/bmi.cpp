5219bded8241c3792c918bbad83302437c98ba3b
크시 <155462542+csiimnida@users.noreply.github.com>
2024-07-23 22:20:46 +0900

    Title: 체질량지수, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240723/체질량지수/bmi.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a,b;
+    double c;
+    cin >> a >> b;
+    c = (10000*b)/(a* a);
+    cout << fiexed;
+    cout.precision(1);
+    cout << c;
+    if(c >= 25){
+        cout << "\nobesity"
+    }
+    return 0;
+}

7bfaf24be09177fe2023d946f0f1c1afe65507e5
크시 <155462542+csiimnida@users.noreply.github.com>
2024-07-23 22:48:04 +0900

    Title: 삼항연산자 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240723/삼항연산자 2/ternary-operator-2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a;
+    char s;
+    cin >> a;
+    s = a == 1 ? 't' : 'f';
+    cout << s;
+    return 0;
+}

8f1bdae1cb8282b6268c89537e4f738c9b695cfc
크시 <155462542+csiimnida@users.noreply.github.com>
2024-07-24 22:34:10 +0900

    Title: 중앙값 구하기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240724/중앙값 구하기/find-the-median.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a,b,c;
+    cin >> a >> b >>c;
+    int[] arraya;
+    arraya[0] = a;
+    arraya[1] = b;
+    arraya[2] = c;
+    Array.Sort(arraya);
+    cout << array[array.Length / 2];
+    return 0;
+}
